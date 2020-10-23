73d60b0516abc32ce50761f59bcbb423a051ebc7
Danil <danil.lyalin@gmail.com>
2020-10-23 17:59:37 +0300

    All scores request implemented

--- ServletTetris/src/main/java/org/spbstu/servlet/ScoresServlet.java
+++ ServletTetris/src/main/java/org/spbstu/servlet/ScoresServlet.java
@@ -1,6 +1,8 @@
 package org.spbstu.servlet;
 
+import com.google.gson.Gson;
 import org.spbstu.database.InitDatabase;
+import org.spbstu.entity.Player;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/scores")
 public class ScoresServlet extends HttpServlet {
@@ -15,8 +18,20 @@ public class ScoresServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        init();
+
+        List<Player> allPlayers = InitDatabase.getDb().getAllScores();
+        String allPlayersJson = new Gson().toJson(allPlayers);
+        req.setAttribute("all", allPlayersJson);
+
         req.getRequestDispatcher("scores.jsp").forward(req, resp);
     }
 
-
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        if (!InitDatabase.checkInit()) {
+            InitDatabase.init();
+        }
+    }
 }
